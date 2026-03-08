ab6d8e7d4b7927884095c48cb293f18f72085dfe
Daniel Mittereder <danielmittereder@gmail.com>
2026-03-08 23:49:23 +0100

    add client api proxy

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,4 +3,14 @@ import solid from 'vite-plugin-solid'
 
 export default defineConfig({
   plugins: [solid()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        // Don't rewrite by default so that /api/* stays /api/*; 
+        // if server expects no prefix, use: rewrite: path => path.replace(/^\/api/, '')
+      },
+    },
+  },
 })
